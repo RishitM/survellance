9858ebc22b48fbe488ecab3e807628b7981b9766
RishitM <72683716+RishitM@users.noreply.github.com>
2021-06-08 19:14:05 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,9 +4,17 @@ vid=createVideo("video.mp4")
 }
 function setup(){
     canvas=createCanvas(400,400)
-    canvas.position(300,300)
+    canvas.position(500,300)
     vid.hide()
+
 }
 function draw(){
 image(vid,0,0,400,400)  
+}
+function start(){
+    object_detector=ml5.objectDetector("cocossd",modelLoaded)
+
+}
+function modelLoaded(){
+    console.log("Model Loaded");
 }
